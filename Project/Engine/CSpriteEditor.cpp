0338eb45ad99d363cd5e694c816d6baab4f204a9
승연 정 <wjdtmd486@naver.com>
2024-01-21 23:03:25 +0900

    chore : CSpriteEditor canvas ImRect로 변경

--- Project/Engine/CSpriteEditor.cpp
+++ Project/Engine/CSpriteEditor.cpp
@@ -34,117 +34,115 @@ void CSpriteEditor::DrawViewprot()
 {
     ImGui::Begin("Viewprot##SpriteEditor");
 
-    if (ImGui::Begin("Canvas"))
+    static ImVector<ImRect> Rects;
+    static ImVec2 scrolling(0.0f, 0.0f);
+    static bool opt_enable_grid = true;
+    static bool opt_enable_context_menu = true;
+    static bool adding_line = false;
+
+    ImGui::Checkbox("Enable grid", &opt_enable_grid);
+    ImGui::Checkbox("Enable context menu", &opt_enable_context_menu);
+    ImGui::Text("Mouse Left: drag to add lines,\nMouse Right: drag to scroll, click for context menu.");
+
+    // Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use
+    // IsItemHovered()/IsItemActive()
+    ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();    // ImDrawList API uses screen coordinates!
+    ImVec2 canvas_sz = ImGui::GetContentRegionAvail(); // Resize canvas to what's available
+    if (canvas_sz.x < 50.0f)
+        canvas_sz.x = 50.0f;
+    if (canvas_sz.y < 50.0f)
+        canvas_sz.y = 50.0f;
+    ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
+
+    // Draw border and background color
+    ImGuiIO& io = ImGui::GetIO();
+    ImDrawList* draw_list = ImGui::GetWindowDrawList();
+    draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
+    draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
+
+    // This will catch our interactions
+    ImGui::InvisibleButton("canvas", canvas_sz, ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
+    const bool is_hovered = ImGui::IsItemHovered();                            // Hovered
+    const bool is_active = ImGui::IsItemActive();                              // Held
+    const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y); // Lock scrolled origin
+    const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x, io.MousePos.y - origin.y);
+
+    // Add first and second point
+    if (is_hovered && !adding_line && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
     {
-        static ImVector<ImVec2> Rects;
-        static ImVec2 scrolling(0.0f, 0.0f);
-        static bool opt_enable_grid = true;
-        static bool opt_enable_context_menu = true;
-        static bool adding_line = false;
+        ImRect rect;
 
-        ImGui::Checkbox("Enable grid", &opt_enable_grid);
-        ImGui::Checkbox("Enable context menu", &opt_enable_context_menu);
-        ImGui::Text("Mouse Left: drag to add lines,\nMouse Right: drag to scroll, click for context menu.");
+        rect.Min = mouse_pos_in_canvas;
+        rect.Max = mouse_pos_in_canvas;
 
-        // Using InvisibleButton() as a convenience 1) it will advance the layout cursor and 2) allows us to use
-        // IsItemHovered()/IsItemActive()
-        ImVec2 canvas_p0 = ImGui::GetCursorScreenPos();    // ImDrawList API uses screen coordinates!
-        ImVec2 canvas_sz = ImGui::GetContentRegionAvail(); // Resize canvas to what's available
-        if (canvas_sz.x < 50.0f)
-            canvas_sz.x = 50.0f;
-        if (canvas_sz.y < 50.0f)
-            canvas_sz.y = 50.0f;
-        ImVec2 canvas_p1 = ImVec2(canvas_p0.x + canvas_sz.x, canvas_p0.y + canvas_sz.y);
-
-        // Draw border and background color
-        ImGuiIO& io = ImGui::GetIO();
-        ImDrawList* draw_list = ImGui::GetWindowDrawList();
-        draw_list->AddRectFilled(canvas_p0, canvas_p1, IM_COL32(50, 50, 50, 255));
-        draw_list->AddRect(canvas_p0, canvas_p1, IM_COL32(255, 255, 255, 255));
-
-        // This will catch our interactions
-        ImGui::InvisibleButton("canvas", canvas_sz,
-                               ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight);
-        const bool is_hovered = ImGui::IsItemHovered();                            // Hovered
-        const bool is_active = ImGui::IsItemActive();                              // Held
-        const ImVec2 origin(canvas_p0.x + scrolling.x, canvas_p0.y + scrolling.y); // Lock scrolled origin
-        const ImVec2 mouse_pos_in_canvas(io.MousePos.x - origin.x, io.MousePos.y - origin.y);
-
-        // Add first and second point
-        if (is_hovered && !adding_line && ImGui::IsMouseClicked(ImGuiMouseButton_Left))
-        {
-            Rects.push_back(mouse_pos_in_canvas);
-            Rects.push_back(mouse_pos_in_canvas);
-            adding_line = true;
-        }
-        if (adding_line)
-        {
-            Rects.back() = mouse_pos_in_canvas;
-            if (!ImGui::IsMouseDown(ImGuiMouseButton_Left))
-                adding_line = false;
-        }
-
-        // Pan (we use a zero mouse threshold when there's no context menu)
-        // You may decide to make that threshold dynamic based on whether the mouse is hovering something etc.
-        const float mouse_threshold_for_pan = opt_enable_context_menu ? -1.0f : 0.0f;
-        if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Right, mouse_threshold_for_pan))
-        {
-            scrolling.x += io.MouseDelta.x;
-            scrolling.y += io.MouseDelta.y;
-        }
-
-        // Context menu (under default mouse threshold)
-        ImVec2 drag_delta = ImGui::GetMouseDragDelta(ImGuiMouseButton_Right);
-        if (opt_enable_context_menu && drag_delta.x == 0.0f && drag_delta.y == 0.0f)
-            ImGui::OpenPopupOnItemClick("context", ImGuiPopupFlags_MouseButtonRight);
-        if (ImGui::BeginPopup("context"))
-        {
-            if (adding_line)
-                Rects.resize(Rects.size() - 2);
-            adding_line = false;
-            if (ImGui::MenuItem("Remove one", NULL, false, Rects.Size > 0))
-            {
-                Rects.resize(Rects.size() - 2);
-            }
-            if (ImGui::MenuItem("Remove all", NULL, false, Rects.Size > 0))
-            {
-                Rects.clear();
-            }
-            ImGui::EndPopup();
-        }
-
-        static float OffsetScale = 1.f;
-        float wheel = ImGui::GetIO().MouseWheel;
-        if (wheel > 0)
-            OffsetScale *= 1.1f;
-        else if (wheel < 0)
-            OffsetScale *= 0.9f;
-
-        // Draw grid + all lines in the canvas
-        draw_list->PushClipRect(canvas_p0, canvas_p1, true);
-        if (opt_enable_grid)
-        {
-            const float GRID_STEP = 64.0f;
-
-            for (float x = fmodf(scrolling.x, GRID_STEP * OffsetScale); x < canvas_sz.x; x += GRID_STEP * OffsetScale)
-                draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y), ImVec2(canvas_p0.x + x, canvas_p1.y),
-                                   IM_COL32(200, 200, 200, 40));
-            for (float y = fmodf(scrolling.y, GRID_STEP * OffsetScale); y < canvas_sz.y; y += GRID_STEP * OffsetScale)
-                draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y),
-                                   IM_COL32(200, 200, 200, 40));
-        }
-
-        if (nullptr != m_pTex.Get())
-            draw_list->AddImage((void*)m_pTex->GetSRV().Get(), origin, origin + ImVec2(500.f, 500.f) * OffsetScale);
-
-        for (int n = 0; n < Rects.Size; n += 2)
-            draw_list->AddRect(ImVec2(origin.x + Rects[n].x, origin.y + Rects[n].y),
-                               ImVec2(origin.x + Rects[n + 1].x, origin.y + Rects[n + 1].y), IM_COL32(255, 0, 0, 255));
-
-        draw_list->PopClipRect();
-
-        ImGui::End();
+        Rects.push_back(rect);
+        adding_line = true;
     }
+    if (adding_line)
+    {
+        Rects.back().Max = mouse_pos_in_canvas;
+        if (!ImGui::IsMouseDown(ImGuiMouseButton_Left))
+            adding_line = false;
+    }
+
+    // Pan (we use a zero mouse threshold when there's no context menu)
+    // You may decide to make that threshold dynamic based on whether the mouse is hovering something etc.
+    const float mouse_threshold_for_pan = opt_enable_context_menu ? -1.0f : 0.0f;
+    if (is_active && ImGui::IsMouseDragging(ImGuiMouseButton_Right, mouse_threshold_for_pan))
+    {
+        scrolling.x += io.MouseDelta.x;
+        scrolling.y += io.MouseDelta.y;
+    }
+
+    // Context menu (under default mouse threshold)
+    ImVec2 drag_delta = ImGui::GetMouseDragDelta(ImGuiMouseButton_Right);
+    if (opt_enable_context_menu && drag_delta.x == 0.0f && drag_delta.y == 0.0f)
+        ImGui::OpenPopupOnItemClick("context", ImGuiPopupFlags_MouseButtonRight);
+    if (ImGui::BeginPopup("context"))
+    {
+        if (adding_line)
+            Rects.resize(Rects.size() - 1);
+        adding_line = false;
+        if (ImGui::MenuItem("Remove one", NULL, false, Rects.Size > 0))
+        {
+            Rects.resize(Rects.size() - 1);
+        }
+        if (ImGui::MenuItem("Remove all", NULL, false, Rects.Size > 0))
+        {
+            Rects.clear();
+        }
+        ImGui::EndPopup();
+    }
+
+    static float OffsetScale = 1.f;
+    float wheel = ImGui::GetIO().MouseWheel;
+    if (wheel > 0)
+        OffsetScale *= 1.1f;
+    else if (wheel < 0)
+        OffsetScale *= 0.9f;
+
+    // Draw grid + all lines in the canvas
+    draw_list->PushClipRect(canvas_p0, canvas_p1, true);
+    if (opt_enable_grid)
+    {
+        const float GRID_STEP = 64.0f;
+
+        for (float x = fmodf(scrolling.x, GRID_STEP * OffsetScale); x < canvas_sz.x; x += GRID_STEP * OffsetScale)
+            draw_list->AddLine(ImVec2(canvas_p0.x + x, canvas_p0.y), ImVec2(canvas_p0.x + x, canvas_p1.y),
+                               IM_COL32(200, 200, 200, 40));
+        for (float y = fmodf(scrolling.y, GRID_STEP * OffsetScale); y < canvas_sz.y; y += GRID_STEP * OffsetScale)
+            draw_list->AddLine(ImVec2(canvas_p0.x, canvas_p0.y + y), ImVec2(canvas_p1.x, canvas_p0.y + y),
+                               IM_COL32(200, 200, 200, 40));
+    }
+
+    if (nullptr != m_pTex.Get())
+        draw_list->AddImage((void*)m_pTex->GetSRV().Get(), origin, origin + ImVec2(500.f, 500.f) * OffsetScale);
+
+    for (int n = 0; n < Rects.Size; n++)
+        draw_list->AddRect(ImVec2(origin.x + Rects[n].Min.x, origin.y + Rects[n].Min.y),
+                           ImVec2(origin.x + Rects[n].Max.x, origin.y + Rects[n].Max.y), IM_COL32(255, 0, 0, 255));
+
+    draw_list->PopClipRect();
 
     ImGui::End();
 }
