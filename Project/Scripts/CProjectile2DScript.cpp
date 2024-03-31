69b1fe7217a837305dc1f688c18114faa4ee8857
승연 정 <wjdtmd486@naver.com>
2024-03-31 16:49:36 +0900

    feat : FlyingBook Enemy 구현

--- Project/Scripts/CProjectile2DScript.cpp
+++ Project/Scripts/CProjectile2DScript.cpp
@@ -1,22 +1,32 @@
 #include "pch.h"
 #include "CProjectile2DScript.h"
+#include "CPlayerScript.h"
+#include "CEnemyScript.h"
 
 CProjectile2DScript::CProjectile2DScript()
     : CScript(PROJECTILE2DSCRIPT)
+    , m_pTarget(nullptr)
     , m_vDir(Vec3(0.f, 0.f, 0.f))
     , m_Speed(0.f)
+    , m_ATK(5)
+    , m_Duration(0.f)
 {
     AddScriptParam(SCRIPT_PARAM::VEC3, &m_vDir, "Direction");
     AddScriptParam(SCRIPT_PARAM::FLOAT, &m_Speed, "Speed");
+    AddScriptParam(SCRIPT_PARAM::INT, &m_ATK, "ATK");
 }
 
 CProjectile2DScript::CProjectile2DScript(const CProjectile2DScript& origin)
     : CScript(origin)
+    , m_pTarget(origin.m_pTarget)
     , m_vDir(origin.m_vDir)
     , m_Speed(origin.m_Speed)
+    , m_ATK(origin.m_ATK)
+    , m_Duration(0.f)
 {
     AddScriptParam(SCRIPT_PARAM::VEC3, &m_vDir, "Base Direction");
     AddScriptParam(SCRIPT_PARAM::FLOAT, &m_Speed, "Speed");
+    AddScriptParam(SCRIPT_PARAM::INT, &m_ATK, "ATK");
 }
 
 CProjectile2DScript::~CProjectile2DScript()
@@ -25,6 +35,10 @@ CProjectile2DScript::~CProjectile2DScript()
 
 void CProjectile2DScript::tick()
 {
+    m_Duration += DT;
+    if (m_Duration > 10.f)
+        GamePlayStatic::DestroyGameObject(GetOwner());
+
     if (nullptr == Rigidbody2D())
         return;
 
@@ -37,6 +51,8 @@ void CProjectile2DScript::SetTarget(CGameObject* _pTarget, Vec3 _vBaseDir)
 {
     if (nullptr == _pTarget)
         return;
+
+    m_pTarget = _pTarget;
 
     Vec3 ProjPos = Transform()->GetRelativePos();
     Vec3 TargetPos = _pTarget->Transform()->GetRelativePos();
@@ -70,14 +86,51 @@ void CProjectile2DScript::SetTarget(CGameObject* _pTarget, Vec3 _vBaseDir)
     Transform()->SetRelativeRotation(rot);
 }
 
+void CProjectile2DScript::OnTriggerEnter(CCollider2D* _OtherCollider)
+{
+    if (nullptr == m_pTarget)
+        return;
+
+    Vec3 ProjectilePos = Transform()->GetWorldPos();
+
+    if (L"Player" == m_pTarget->GetName())
+    {
+        // 플레이어 타겟 투사체
+        CPlayerScript* pPlayerScript = _OtherCollider->GetOwner()->GetScript<CPlayerScript>();
+        if (nullptr == pPlayerScript)
+            return;
+
+        Vec3 PlayerPos = _OtherCollider->GetOwner()->Transform()->GetWorldPos();
+        ProjectilePos.z = 0.f;
+        PlayerPos.z = 0.f;
+        pPlayerScript->TakeHit(m_ATK, (PlayerPos - ProjectilePos).Normalize());
+        GamePlayStatic::DestroyGameObject(GetOwner());
+    }
+    else
+    {
+        // 몬스터 타겟 투사체
+        CEnemyScript* pEnemyScript = _OtherCollider->GetOwner()->GetScript<CEnemyScript>();
+        if (nullptr == pEnemyScript)
+            return;
+
+        Vec3 EnemyPos = _OtherCollider->GetOwner()->Transform()->GetWorldPos();
+        ProjectilePos.z = 0.f;
+        EnemyPos.z = 0.f;
+        pEnemyScript->TakeHit(m_ATK, (EnemyPos - ProjectilePos).Normalize());
+        GamePlayStatic::DestroyGameObject(GetOwner());
+    }
+}
+
 void CProjectile2DScript::SaveToLevelFile(FILE* _File)
 {
     fwrite(&m_vDir, sizeof(Vec3), 1, _File);
     fwrite(&m_Speed, sizeof(float), 1, _File);
+    fwrite(&m_ATK, sizeof(int), 1, _File);
 }
 
 void CProjectile2DScript::LoadFromLevelFile(FILE* _File)
 {
     fread(&m_vDir, sizeof(Vec3), 1, _File);
     fread(&m_Speed, sizeof(float), 1, _File);
+    fread(&m_ATK, sizeof(int), 1, _File);
 }
--- Project/Scripts/CProjectile2DScript.h
+++ Project/Scripts/CProjectile2DScript.h
@@ -4,14 +4,20 @@
 class CProjectile2DScript : public CScript
 {
 private:
+    CGameObject* m_pTarget;
     Vec3 m_vDir;
     float m_Speed;
+    int m_ATK;
+    float m_Duration;
 
 private:
     virtual void tick() override;
 
 public:
     void SetTarget(CGameObject* _pTarget, Vec3 _vBaseDir);
+
+private:
+    virtual void OnTriggerEnter(CCollider2D* _OtherCollider) override;
 
 public:
     virtual void SaveToLevelFile(FILE* _File) override;
