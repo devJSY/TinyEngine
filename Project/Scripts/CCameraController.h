189a5066f57c6bd41866665fce38ca0a59f9f682
창희 한 <sjrnfl0412@gmail.com>
2024-07-27 05:28:15 +0900

    feat. Camera Edit Mode 추가 - 플레이중 실시간으로 카메라의 각도, 거리 조절이 가능하도록 구현 - W,A,S,D키를 이용하여 경우 현재 카메라를 기준으로 상,하,좌,우로 회전 - Q,E 키를 이용하여 카메라의 Dist가 조정됨 - Tab키를 이용하여 EditMode Toggle - 회전 속도와 카메라 거리는 스크립트 파라미터를 통해 직접 조정할 수 있도록 함 - 추가된 Script 파라미터에 대한 Save, Load 적용 - 기존에 tick 함수 안에 있던 단계를 각각의 함수로 빼주었음

--- Project/Scripts/CCameraController.h
+++ Project/Scripts/CCameraController.h
@@ -14,8 +14,8 @@ private:
     CGameObject*                m_Target;               // 카메라가 바라봐야하는 타겟
 
     // 설정 값
-    Vec3                        m_LookDir;              // 카메라가 바라보는 각도
     Vec3                        m_LookEyePos;           // 카메라가 있어야 하는 위치
+    Vec3                        m_LookDir;              // 카메라가 바라보는 각도
     float                       m_LookDist;             // 카메라와 타겟 사이의 거리
 
     float                       m_MinSpeed;             // 카메라의 최소 스피드
@@ -26,21 +26,31 @@ private:
 
     // 현재 프레임
     Vec3                        m_CurLookDir;           // 현재 프레임에서 카메라의 각도
-    Vec3                        m_CurCamPos;            // 현재 프레임에서 카메라의 위치
     Vec3                        m_CurLookAtPos;         // 현재 프레임에서 카메라가 바라봐야하는 위치
+    Vec3                        m_TargetPos;            // 현재 프레임에서 타겟의 위치
 
     // 이전 프레임
     Vec3                        m_PrevLookDir;          // 이전 프레임에서 카메라의 각도
-    Vec3                        m_PrevCamPos;           // 이전 프레임에서 카메라의 위치
     Vec3                        m_PrevLookAtPos;        // 이전 프레임에서 카메라가 바라봐야하는 위치
+
+    //Edit
+    bool                        m_EditMode;             // EditMode 스위치
+    float                       m_EditRotSpeed;         // EditMode에서의 카메라 회전 조절 속도
+    float                       m_EditZoomSpeed;        // EditMode에서의 카메라 거리 조절 속도
 
 public:
     virtual void begin() override;
     virtual void tick() override;
 
 private:
+    void UpdateTargetPos(); 
+    void UpdateLookAtPos();
+    void UpdateLookDir();
+    
     Vec3 CalCamPos(Vec3 _TargetWorldPos, Vec3 _LookDir, float _CamDist);
-    float GetAngleBetweenVectors(Vec3 _V1, Vec3 _V2);
+    
+    // EditMode
+    void EditMode();
 
 
 public:
