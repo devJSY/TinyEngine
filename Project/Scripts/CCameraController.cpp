7023841b5386ba18006f0559c770ba515718d5a0
창희 한 <sjrnfl0412@gmail.com>
2024-07-28 05:21:59 +0900

    feat. CameraController Progress Setup 구현, 카메라 Offset 추가 - 특정 구역안에서는 구역안에서의 진행 정도(위치)에 따라 특정 각도, 거리를 가지도록 카메라를 제어) - 카메라가 타겟이 아니라 타겟+Offset의 위치를 바라보도록 수

--- Project/Scripts/CCameraController.cpp
+++ Project/Scripts/CCameraController.cpp
@@ -5,7 +5,9 @@
 
 CCameraController::CCameraController()
     : CScript(CAMERACONTROLLER)
+    , m_CurSetup(CameraSetup::NORMAL)
     , m_Target(nullptr)
+    , m_TargetPos(Vec3(0.f,0.f,0.f))
     , m_LookDir(0.f, 0.f, 0.f)
     , m_LookDist(0.f)
     , m_RotationSpeed(50.f)
@@ -13,14 +15,17 @@ CCameraController::CCameraController()
     , m_EditZoomSpeed(300.f)
     , m_EditMode(false)
 {
-    AddScriptParam(SCRIPT_PARAM::VEC3, &m_LookDir, "Init Look Dir", 0.05f);
-    AddScriptParam(SCRIPT_PARAM::FLOAT, &m_LookDist, "Init Look Dis", 0.05f);
+
+    AddScriptParam(SCRIPT_PARAM::VEC3, &m_LookDir, "Offset");
+    AddScriptParam(SCRIPT_PARAM::VEC3, &m_LookDir, "Look Dir", 0.05f);
+    AddScriptParam(SCRIPT_PARAM::FLOAT, &m_LookDist, "Look Distance", 1.f);
 
     AddScriptParam(SCRIPT_PARAM::FLOAT, &m_MinSpeed, "MinSpeed");
     AddScriptParam(SCRIPT_PARAM::FLOAT, &m_MaxSpeed, "MaxSpeed");
     AddScriptParam(SCRIPT_PARAM::FLOAT, &m_ThresholdDistance, "Threshold Distance");
 
     AddScriptParam(SCRIPT_PARAM::FLOAT, &m_RotationSpeed, "Rotation Speed");
+    AddScriptParam(SCRIPT_PARAM::FLOAT, &m_ZoomSpeed, "Zoom Speed");
 
     // Edit Mode
     AddScriptParam(SCRIPT_PARAM::BOOL, &m_EditMode, "Edit Mode");
@@ -55,11 +60,14 @@ void CCameraController::begin()
 
     // 카메라 세팅
     m_TargetPos = m_Target->Transform()->GetWorldPos();
+    m_TargetPos += m_Offset;
 
     m_PrevLookDir = m_LookDir;
     m_CurLookDir = m_LookDir;
     m_PrevLookAtPos = m_TargetPos;
     m_CurLookAtPos = m_TargetPos;
+    m_PrevDistance = m_LookDist;
+    m_CurDistance = m_LookDist;
 
     Vec3 InitPos = CalCamPos(m_TargetPos, m_LookDir, m_LookDist);
     
@@ -67,6 +75,16 @@ void CCameraController::begin()
 
     Transform()->SetWorldPos(InitPos);
     Transform()->SetDirection(m_LookDir);
+
+    // Test
+    m_ProgressStartPos = Vec3(0.f, 0.f, 0.f);
+    m_ProgressStartDir = Vec3(0.f, -1.f, 1.f);
+    m_ProgressStartDist = 1000.f;
+    m_ProgressEndPos = Vec3(0.f, 0.f, 3000.f);
+    m_ProgressEndDir = Vec3(-1.f, -1.f, 0.f);
+    m_ProgressEndDist = 2000.f;
+
+    m_CurSetup = CameraSetup::PROGRESS;
 }
 
 void CCameraController::tick()
@@ -80,14 +98,21 @@ void CCameraController::tick()
     // ========================= Prev Data Save =========================
     m_PrevLookDir = m_CurLookDir;
     m_PrevLookAtPos = m_CurLookAtPos;
+    m_PrevDistance = m_CurDistance;
 
-    // Camera의 위치 계산을 위해 Target, LookAtPos, LookDir을 업데이트한다.
+    // ========================= Target Update ==========================
     UpdateTargetPos();
+
+    // 현재 Setup에 맞게 카메라의 LookDir, LookDist를 수정한다.
+    SetUpProc();
+
+    // LookDir, LookDist에 맞게 LookAtPos, LookDir을 업데이트한다.
     UpdateLookAtPos();
     UpdateLookDir();
+    UpdateLookDistance();
 
     // ========================= Eye Pos Update =========================
-    m_LookEyePos = CalCamPos(m_CurLookAtPos, m_CurLookDir, m_LookDist);
+    m_LookEyePos = CalCamPos(m_CurLookAtPos, m_CurLookDir, m_CurDistance);
 
     // ==================== Camera Transform Update ====================
     Transform()->SetDirection(m_CurLookDir);
@@ -100,10 +125,25 @@ void CCameraController::tick()
     GamePlayStatic::DrawDebugLine(Transform()->GetWorldPos(), m_CurLookDir, m_LookDist, Vec3(0.f, 0.f, 0.f), true);
 }
 
+void CCameraController::SetUpProc()
+{
+    switch (m_CurSetup)
+    {
+    case CameraSetup::NORMAL:
+        break;
+    case CameraSetup::PROGRESS:
+        Progress();
+        break;
+    default:
+        break;
+    }
+}
+
 void CCameraController::UpdateTargetPos()
 {
     // 타겟의 현재 위치 업데이트
     m_TargetPos = m_Target->Transform()->GetWorldPos();
+    m_TargetPos += m_Offset;
 }
 
 void CCameraController::UpdateLookAtPos()
@@ -160,6 +200,21 @@ void CCameraController::UpdateLookDir()
             Quat SlerpQuat = Quat::Slerp(PrevQuat, LookQuat, t);
             m_CurLookDir = QuaternionToVector(SlerpQuat);
         }
+    }
+}
+
+void CCameraController::UpdateLookDistance()
+{
+    // 이전 프레임의 거리가 목표하는 거리보다 크다면
+    if (m_PrevDistance > m_LookDist)
+    {
+        m_CurDistance = m_PrevDistance - m_ZoomSpeed * DT;
+    }
+
+    // 이전 프레임의 거리가 목표하는 거리보다 작다면
+    else if (m_PrevDistance < m_LookDist)
+    {
+        m_CurDistance = m_PrevDistance + m_ZoomSpeed * DT;
     }
 }
 
@@ -221,6 +276,34 @@ void CCameraController::EditMode()
             m_LookDist -= m_EditZoomSpeed * DT;
         }
     }
+}
+
+void CCameraController::Progress()
+{    
+    // Start와 End 사이에서 LookAtPos에 수직인 점을 찾기
+    Vec3 ProgressRoad = m_ProgressEndPos - m_ProgressStartPos;
+    Vec3 ToPrevLookAt = m_PrevLookAtPos - m_ProgressStartPos;
+
+    float DotRes = ToPrevLookAt.Dot(ProgressRoad);
+    float LengthSquared = ProgressRoad.Dot(ProgressRoad);
+
+    // 아직 카메라가 바라봐야할 곳이 시작점 이전이거나, 끝점을 넘어갔을 경우 진행 X
+    if (DotRes <= 0.f || DotRes > LengthSquared)
+    {
+        return;
+    }
+
+   float t = DotRes / LengthSquared;
+   
+   // t에 맞게 각 값들을 보간한다.
+   m_ProgressStartDir.Normalize();
+   m_ProgressEndDir.Normalize();
+
+   Quat StartQuat = VectorToQuaternion(m_ProgressStartDir);
+   Quat EndQuat = VectorToQuaternion(m_ProgressEndDir);
+   Quat SlerpQuat = Quat::Slerp(StartQuat, EndQuat, t);
+   m_LookDir = QuaternionToVector(SlerpQuat);
+   m_LookDist = Lerp(m_ProgressStartDist, m_ProgressEndDist, t);
 }
 
 Vec3 CCameraController::CalCamPos(Vec3 _TargetWorldPos, Vec3 _LookDir, float _CamDist)
--- Project/Scripts/CCameraController.h
+++ Project/Scripts/CCameraController.h
@@ -2,16 +2,21 @@
 
 #include <Engine/CScript.h>
 
-enum class CameraMode
+enum class CameraSetup
 {
     NORMAL,
-
+    PROGRESS,
 };
 
 class CCameraController : public CScript
 {
 private:
+    CameraSetup                 m_CurSetup;             // 현재 카메라의 Setup
+
+    // Target
     CGameObject*                m_Target;               // 카메라가 바라봐야하는 타겟
+    Vec3                        m_TargetPos;            // 현재 프레임에서 타겟의 위치
+    Vec3                        m_Offset;               // 타겟으로부터의 오프셋
 
     // 설정 값
     Vec3                        m_LookEyePos;           // 카메라가 있어야 하는 위치
@@ -23,16 +28,26 @@ private:
     float                       m_ThresholdDistance;    // 카메라가 최대 스피드로 이동하기 위한 임계 거리
 
     float                       m_RotationSpeed;        // 회전 속도
+    float                       m_ZoomSpeed;            // Zoom 속도
 
     // 현재 프레임
     Vec3                        m_CurLookDir;           // 현재 프레임에서 카메라의 각도
     Vec3                        m_CurLookAtPos;         // 현재 프레임에서 카메라가 바라봐야하는 위치
-    Vec3                        m_TargetPos;            // 현재 프레임에서 타겟의 위치
+    float                       m_CurDistance;          // 현재 프레임에서 카메라와 LookAt의 거리
 
     // 이전 프레임
     Vec3                        m_PrevLookDir;          // 이전 프레임에서 카메라의 각도
     Vec3                        m_PrevLookAtPos;        // 이전 프레임에서 카메라가 바라봐야하는 위치
+    float                       m_PrevDistance;         // 이전 프레임에서 카메라와 LookAt의 거리
 
+    // Progress Setup
+    Vec3                        m_ProgressStartPos;
+    Vec3                        m_ProgressStartDir;
+    float                       m_ProgressStartDist;
+    Vec3                        m_ProgressEndPos;
+    Vec3                        m_ProgressEndDir;
+    float                       m_ProgressEndDist;
+    
     //Edit
     bool                        m_EditMode;             // EditMode 스위치
     float                       m_EditRotSpeed;         // EditMode에서의 카메라 회전 조절 속도
@@ -42,15 +57,24 @@ public:
     virtual void begin() override;
     virtual void tick() override;
 
+
 private:
+    void SetUpProc();
     void UpdateTargetPos(); 
     void UpdateLookAtPos();
     void UpdateLookDir();
+    void UpdateLookDistance();
     
     Vec3 CalCamPos(Vec3 _TargetWorldPos, Vec3 _LookDir, float _CamDist);
     
     // EditMode
     void EditMode();
+    void Progress();
+
+public:
+    //void ChangeTarget();
+    //void ChangeSetting(Vec3 _LookDir, float CamDist, );
+    //void ProgressSetup(Vec3 _StartPos, Vec3 _EndPos, Vec3 _StartDir, Vec3 _EndDir, float _StartDist, float _EndDist);
 
 
 public:
