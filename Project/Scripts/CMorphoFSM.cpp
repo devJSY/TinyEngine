353a4b0fbff79525e875d62c7135b462c06bf88f
DV <c1oudcat@naver.com>
2024-08-16 01:59:02 +0900

    fix : Morpho Jump 힘 조절

--- Project/Scripts/CMorphoFSM.cpp
+++ Project/Scripts/CMorphoFSM.cpp
@@ -146,7 +146,7 @@ void CMorphoFSM::tick()
 
     if (KEY_TAP(KEY::ENTER))
     {
-        ChangeStateGroup(MorphoStateGroup::AtkGroundNormalFar, L"ATKG_NORMALFAR_SWORDSLASH");
+        ChangeStateGroup(MorphoStateGroup::MoveToGround, L"MOVEG_JUMP");
         //ChangeStateGroup(MorphoStateGroup::DEMO, L"DEMO_APPEAR");
     }
 
