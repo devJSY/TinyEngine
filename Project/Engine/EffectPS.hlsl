d8c66963bc890f6a981b198078298e9ba86ab522
승연 정 <wjdtmd486@naver.com>
2024-08-24 23:24:18 +0900

    fix : GameObject Load 시 부모 오브젝트가 없는데 본 인덱스가 존재하는 경우 예외처리 추가 - 본 소켓에 붙은 오브젝트를 프리팹으로 저장 시 부모는 없는데 본 인덱스가 남아있어서 로딩시 터지는 버그 수정

--- Project/Engine/EffectPS.hlsl
+++ Project/Engine/EffectPS.hlsl
@@ -14,7 +14,7 @@ float4 main(PS_IN input) : SV_Target
 
     if (g_btex_0)
     {
-        float4 SampleColor = Texture.Sample(g_LinearWrapSampler, input.vUV0 + (float2(g_Time, sin(g_Time)) * Speed));
+        float4 SampleColor = Texture.Sample(g_LinearWrapSampler, input.vUV0 + float2(g_Time, g_Time) * float2(Speed, Speed));
         vOutColor.rgb = lerp(Color1.rgb, Color2.rgb, input.vUV0.x);
         vOutColor.rgb *= SampleColor.rgb;
         vOutColor.a = SampleColor.a;
