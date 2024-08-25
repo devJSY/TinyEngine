c1b8d8ad6258cdc7e1dc6d4ccc99a1a1b2aff1bd
DV <c1oudcat@naver.com>
2024-08-26 04:55:02 +0900

    fix : Stuffed 진입 시 공격키 Pressed된 상태로 조작하면 바로 뱉는 현상 수정

--- Project/Scripts/CKirbyStuffedJumpFall.cpp
+++ Project/Scripts/CKirbyStuffedJumpFall.cpp
@@ -12,7 +12,7 @@ CKirbyStuffedJumpFall::~CKirbyStuffedJumpFall()
 void CKirbyStuffedJumpFall::tick()
 {
     // Change State
-    if (KEY_TAP(KEY_ATK) || KEY_PRESSED(KEY_ATK))
+    if (KEY_TAP(KEY_ATK))
     {
         ChangeState(L"ATTACK");
     }
