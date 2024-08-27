52d108b76ada4465c3186beec7bd4b18777b2127
DV <c1oudcat@naver.com>
2024-08-28 04:05:26 +0900

    fix : BossDropStar 예외처리 추가 & 추가구현 - Boss 있는 경우에만 Init Force 추가 - Scale 0 되지 않도록 세팅 - Init Rot 랜덤초기화 - RotSpeed 랜덤 세팅 - LifeTime 변경

--- Project/Scripts/CElfilisArrowScript.cpp
+++ Project/Scripts/CElfilisArrowScript.cpp
@@ -18,7 +18,7 @@ CElfilisArrowScript::~CElfilisArrowScript()
 void CElfilisArrowScript::begin()
 {
     GetOwner()->SetActive(false);
-    GetOwner()->Transform()->SetLocalScale(Vec3());
+    GetOwner()->Transform()->SetLocalScale(Vec3(0.001f));
 }
 
 void CElfilisArrowScript::tick()
