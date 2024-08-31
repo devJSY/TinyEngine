d31312f435e8123f2e8fa5bf958103f588fa9bbf
DV <c1oudcat@naver.com>
2024-09-01 08:03:24 +0900

    ING : Elfilis Body Shader 작성중

--- Project/Engine/CAssetMgr_Kirby.cpp
+++ Project/Engine/CAssetMgr_Kirby.cpp
@@ -230,6 +230,41 @@ void CAssetMgr::CreateDefaultGraphicsShader_Kirby()
     }
 
     // =================================
+    // Elfilis Body Shader
+    // =================================
+    {
+        Ptr<CGraphicsShader> pShader = new CGraphicsShader;
+        pShader->CreateVertexShader(L"shader\\UnrealPBRVS.hlsl", "main");
+        pShader->CreatePixelShader(L"shader\\ElfilisBodyPS.hlsl", "main");
+
+        pShader->SetRSType(RS_TYPE::CULL_BACK);
+        pShader->SetDSType(DS_TYPE::LESS);
+        pShader->SetBSType(BS_TYPE::ALPHA_BLEND);
+
+        pShader->SetDomain(SHADER_DOMAIN::DOMAIN_TRANSPARENT);
+
+        pShader->AddScalarParam(INT_3, "Teleport Flag");
+        pShader->AddScalarParam(FLOAT_3, "Teleport Y");
+
+        pShader->AddScalarParam(INT_0, "Invert NormalMapY");
+        pShader->AddScalarParam(FLOAT_0, "HeightMapping Scale", 0.1f);
+        pShader->AddScalarParam(FLOAT_1, "Rim Power");
+        pShader->AddScalarParam(VEC4_0, "Rim Color");
+
+        pShader->AddTexParam(TEX_0, "Albedo0 Texture");
+        pShader->AddTexParam(TEX_1, "Albedo1 Texture");
+        pShader->AddTexParam(TEX_2, "Albedo2 Texture");
+        pShader->AddTexParam(TEX_3, "Albedo3 Texture");
+        pShader->AddTexParam(TEX_4, "MRA Texture"); // Metallic, Roughness, Ambient Occlusion
+        pShader->AddTexParam(TEX_5, "Normal Texture");
+        pShader->AddTexParam(TEX_6, "Height Texture");
+        pShader->AddTexParam(TEX_7, "Emissive Texture");
+
+        pShader->SetName(L"ElfilisBodyShader");
+        AddAsset(L"ElfilisBodyShader", pShader);
+    }
+    
+    // =================================
     // Transparent Elfilis Shader
     // =================================
     {
